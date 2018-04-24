4ca5617643c1102e2bf3076b8728901ebfae6629
Carlos <carlosyllana@atmira.com>
2018-04-25 00:54:49 +0200

    Agregar archivos de proyecto.

---
+++ Client.MVC/Controllers/UserMvcController.cs
@@ -0,0 +1,99 @@
+﻿using Client.MVC.Models;
+using Client.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.MVC.Controllers
+{
+    public class UserMvcController : Controller
+    {
+        // GET: UserMvc
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UserMvc/Details
+        public ActionResult Details()
+        {
+            UserService uservice = new UserService();
+            var model2 = uservice.GetAsync();
+
+            return View(model2.Result);
+        }
+
+        // GET: UserMvc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserMvc/Create
+        [HttpPost]
+        public ActionResult Create(UserServiceModel usuarioServiceModeln)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                UserService uservice = new UserService();
+                var model = uservice.SetAsync(usuarioServiceModeln).Result; ;
+                UserMvc uMvc = new UserMvc();
+                uMvc.Id = model.Id;
+                uMvc.Nombre = model.Nombre;
+                uMvc.Apellidos = model.Apellidos;
+                return View(model);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserMvc/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserMvc/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserMvc/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserMvc/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Client.Service/UserService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _cliente;
+
+        public UserService()
+        {
+           
+            _cliente = ServiceConfiguration.InitClient(new HttpClient());
+
+        }
+
+        public async Task<UserServiceModel> GetAsync()
+        {
+            try
+            {
+                var response = await _cliente.GetAsync("/api/v1/UserAsync/GetAsync",HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+                using (var content = response.Content)
+                {
+                    var x = await response.Content.ReadAsAsync<UserServiceModel>();
+                    return x;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<UserServiceModel> SetAsync(UserServiceModel user)
+        {
+            try
+            {
+
+                var response = await _cliente.PostAsJsonAsync<UserServiceModel>("/api/v1/UserAsync/AddAsync", user ).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+                using (var Content = response.Content)
+                {
+                    return await Content.ReadAsAsync<UserServiceModel>();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
